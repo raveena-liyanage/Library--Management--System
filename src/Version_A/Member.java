c1eb0bdcb76bd96a47739ed92f721bd18cfd1649
raveena-liyanage <raveenaliyanage1@gmail.com>
2026-07-11 00:43:42 +0530

    Remove duplicate line

--- src/Version_A/Member.java
+++ src/Version_A/Member.java
@@ -2,34 +2,33 @@ package Version_A;
 import java.util.ArrayList;
 
 public class Member {
-    public class Member {
-        private String memberId;
-        private String name;
-        private ArrayList<Book> borrowedBooks;
+    private String memberId;
+    private String name;
+    private ArrayList<Book> borrowedBooks;
 
-        public Member(String memberId, String name) {
-            this.memberId = memberId;
-            this.name = name;
-            this.borrowedBooks = new ArrayList<>();
-        }
-        public String getMemberId() {
-            return memberId;
-        }
-        public String getName() {
-            return name;
-        }
-        public ArrayList<Book> getBorrowedBooks() {
-            return borrowedBooks;
-        }
-        public void borrowBook(Book book) {
-            borrowedBooks.add(book);
-        }
-        public void returnBook(Book book) {
-            borrowedBooks.remove(book);
-        }
-        public void displayInfo() {
-            System.out.println("Member ID: " + memberId + " | Name: " + name + " | Books Borrowed: " + borrowedBooks.size());
-        }
+    public Member(String memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+    public String getMemberId() {
+        return memberId;
+    }
+    public String getName() {
+        return name;
+    }
+    public ArrayList<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+    public void displayInfo() {
+        System.out.println("Member ID: " + memberId + " | Name: " + name + " | Books Borrowed: " + borrowedBooks.size());
+    }
 
 
 }
