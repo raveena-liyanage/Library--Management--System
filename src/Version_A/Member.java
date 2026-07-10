922aaa5f94ccc1f35c85ebd99a3aa634d1ea33a2
raveena-liyanage <raveenaliyanage1@gmail.com>
2026-07-11 00:26:13 +0530

    Delete borrow book method

--- src/Version_A/Member.java
+++ src/Version_A/Member.java
@@ -18,8 +18,6 @@ public class Member {
         public String getName() {
             return name;
         }
-        public ArrayList<Book> getBorrowedBooks() {
-            return borrowedBooks;
-        }
+
 
 }
