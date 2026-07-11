59057ac805e9f3a26f3d46eaa71589bdfff39f48
raveena-liyanage <raveenaliyanage1@gmail.com>
2026-07-11 13:14:33 +0530

    Initialize LibrarySystem book and member lists

--- src/Version_A/LibrarySystem.java
+++ src/Version_A/LibrarySystem.java
@@ -3,4 +3,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LibrarySystem {
+    private ArrayList<Book> bookList = new ArrayList<>();
+    private ArrayList<Member> memberList = new ArrayList<>();
 }
