5ed00fb2e185d2293598ab3b3e3ef0d070991d63
raveena-liyanage <raveenaliyanage1@gmail.com>
2026-07-13 20:38:55 +0530

    Add a constructor

--- src/Version_B/Member.java
+++ src/Version_B/Member.java
@@ -4,8 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class Member {
+    //using AI
     private static final int MAX_BOOKS_ALLOWED = 3;
     private final String memberId;
     private final String name;
     private final List<Book> borrowedBooks;
+
+    public Member(String memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
 }
