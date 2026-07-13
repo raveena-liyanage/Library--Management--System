0256f8ac79aa6f42ba249c5f8ea8345b49d371c6
raveena-liyanage <raveenaliyanage1@gmail.com>
2026-07-13 19:13:03 +0530

    Declairedvariables

--- src/Version_B/Book.java
+++ src/Version_B/Book.java
@@ -1,4 +1,11 @@
 package Version_B;
 
+//written manually
 public class Book {
+    private final String isbn;
+    private final String title;
+    private final String author;
+
+    // using AI
+    private boolean available;
 }
