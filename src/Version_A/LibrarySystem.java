e9c507b50c6f8ee71b04e16f140077ce31c67a08
raveena-liyanage <raveenaliyanage1@gmail.com>
2026-07-13 14:37:01 +0530

    Declare choice variable

--- src/Version_A/LibrarySystem.java
+++ src/Version_A/LibrarySystem.java
@@ -28,5 +28,6 @@ public class LibrarySystem {
 public static void main(String[] args) {
     LibrarySystem library = new LibrarySystem();
     Scanner scanner = new Scanner(System.in);
+    int choice;
 
 }
