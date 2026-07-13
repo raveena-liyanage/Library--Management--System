4ab7eaf87643c7318a3dafc326e22aa11fa34c3f
raveena-liyanage <raveenaliyanage1@gmail.com>
2026-07-13 21:09:26 +0530

    Add exceptions

--- src/Version_B/LibraryExceptions.java
+++ src/Version_B/LibraryExceptions.java
@@ -1,2 +1,32 @@
 package Version_B;
 
+//Using AI
+class DuplicateEntryException extends Exception {
+    public DuplicateEntryException(String message) {
+        super(message);
+    }
+}
+
+class BookNotFoundException extends Exception {
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+}
+
+class MemberNotFoundException extends Exception {
+    public MemberNotFoundException(String message) {
+        super(message);
+    }
+}
+
+class BookNotAvailableException extends Exception {
+    public BookNotAvailableException(String message) {
+        super(message);
+    }
+}
+
+class BookNotBorrowedException extends Exception {
+    public BookNotBorrowedException(String message) {
+        super(message);
+    }
+}
