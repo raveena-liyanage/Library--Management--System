55012486a4798b228a2cb6bcac9248954ad85682
raveena-liyanage <raveenaliyanage1@gmail.com>
2026-07-14 16:18:17 +0530

    Declared variables

--- src/Version_B/LibrarySystem.java
+++ src/Version_B/LibrarySystem.java
@@ -3,6 +3,8 @@ import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
-
+// AI Generated
 public class LibrarySystem {
+    private final Map<String, Book> books = new HashMap<>();
+    private final Map<String, Member> members = new HashMap<>();
 }
